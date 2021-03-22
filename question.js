4824671d2142f944cbcb636538c21f5831913110
Clay Jones <cgjones0711@gmail.com>
2021-03-22 14:12:59 -0500

    working timer

--- question.js
+++ question.js
@@ -1,2 +1,31 @@
-let questionarray = ["question1 ", "question2" ,"question3" , "question4" ]
+let startButton = document.querySelector("#start");
+let timeEl = document.querySelector(".countDown");
+let mainEl = document.getElementById("main");
 
+
+var secondsLeft = 10;
+setTime();
+function setTime() {
+  // Sets interval in variable
+  var timerInterval = setInterval(function() {
+    secondsLeft--;
+    countDown.textContent = secondsLeft + " Game Over!";
+
+    if(secondsLeft === 0) {
+      // Stops execution of action at set interval
+      clearInterval(timerInterval);
+      // Calls function to create and append image
+      sendMessage();
+    }
+
+  
+},1000);
+
+
+
+
+
+
+}
+
+//document.createElement
