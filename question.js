e64fdba36c1237f758cfb83ff2fa5069a0a91c81
Clay Jones <cgjones0711@gmail.com>
2021-03-22 17:48:57 -0500

    fixed bubble space

--- question.js
+++ question.js
@@ -1,60 +1,40 @@
 let startButton = document.querySelector("#start");
-let timeEl = document.querySelector(".countDown");
-let mainEl = document.getElementById("main");
+let timeEl = document.querySelector("#countDown");
+let mainEl = document.getElementById("#main");
 
 
 
 
 var secondsLeft = 10;
+var gameOver = 0;
 setTime();
 function setTime() {
   // Sets interval in variable
   var timerInterval = setInterval(function() {
     secondsLeft--;
-    countDown.textContent = secondsLeft + " Game Over!";
+    countDown.textContent = secondsLeft + " Time Left!";
 
     if(secondsLeft === 0) {
       // Stops execution of action at set interval
       clearInterval(timerInterval);
-      // Calls function to create and append image
-      sendMessage();
+      
+      
     }
 
-  
+//Start Game
+function startGame() {
+  isWin = false;
+  countDown = 10;
+  // Prevents start button from being clicked when round is in progress
+  startButton.disabled = true;
+  renderBlanks()
+  startTimer()
+}
+
+   
 },1000);
 
 //questions 
-
-  {
-    question: "Which of the following tag represents a generic document or application section in HTML5?",
-    answers: {
-      a: "Section",
-      b: "article",
-      c: "header"
-    },
-    correctAnswer: "c"
-  },
-  {
-    question: "Which of the following tag represents the header of a section in HTML5?",
-    answers: {
-      a: "header",
-      b: "article",
-      c: "aside"
-    },
-    correctAnswer: "a"
-  },
-  {
-    question: "Which of the following input control is used for input fields that should contain an URL address in Web Form 2.0?",
-    answers: {
-      a: "number",
-      b: "URL",
-      c: "RequireJS",
-      d: "range"
-    },
-    correctAnswer: "b"
-  }
-];
-
 
 
 
