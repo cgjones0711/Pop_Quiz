ded20ccc93b614f754b075f65896448dd0e25903
Clay Jones <cgjones0711@gmail.com>
2021-03-22 16:33:47 -0500

    question buttons css

--- question.js
+++ question.js
@@ -3,6 +3,8 @@ let timeEl = document.querySelector(".countDown");
 let mainEl = document.getElementById("main");
 
 
+
+
 var secondsLeft = 10;
 setTime();
 function setTime() {
@@ -20,6 +22,38 @@ function setTime() {
 
   
 },1000);
+
+//questions 
+
+  {
+    question: "Which of the following tag represents a generic document or application section in HTML5?",
+    answers: {
+      a: "Section",
+      b: "article",
+      c: "header"
+    },
+    correctAnswer: "c"
+  },
+  {
+    question: "Which of the following tag represents the header of a section in HTML5?",
+    answers: {
+      a: "header",
+      b: "article",
+      c: "aside"
+    },
+    correctAnswer: "a"
+  },
+  {
+    question: "Which of the following input control is used for input fields that should contain an URL address in Web Form 2.0?",
+    answers: {
+      a: "number",
+      b: "URL",
+      c: "RequireJS",
+      d: "range"
+    },
+    correctAnswer: "b"
+  }
+];
 
 
 
