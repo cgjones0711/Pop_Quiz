e47891974099a6aa6d4a816a006b2ee8a02ad854
Clay Jones <cgjones0711@gmail.com>
2021-03-22 20:49:17 -0500

    added eventlistener

--- questions.js
+++ questions.js
@@ -1,16 +1,15 @@
-let startButton = document.querySelector("#start");
 let timeEl = document.querySelector("#countDown");
 let mainEl = document.getElementById("#main");
 
 
 
 
-var secondsLeft = 10;
-var gameOver = 0;
+let secondsLeft = 45;
+let gameOver = 0;
 setTime();
 function setTime() {
   // Sets interval in variable
-  var timerInterval = setInterval(function() {
+  let timerInterval = setInterval(function() {
     secondsLeft--;
     countDown.textContent = secondsLeft + " Time Left!";
 
@@ -21,25 +20,12 @@ function setTime() {
       
     }
 
-//Start Game
-function startGame() {
-  isWin = false;
-  countDown = 10;
-  // Prevents start button from being clicked when round is in progress
-  startButton.disabled = true;
-  renderBlanks()
-  startTimer()
-}
-
-   
 },1000);
 
-//questions 
+
 
 
 
 
 
 }
-
-//document.createElement
